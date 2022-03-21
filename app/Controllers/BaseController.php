15ce86f3c68c00626747561edf5fddced1849269
Ivan Mercedes <ivanmercede@gmail.com>
2022-03-21 10:09:19 -0400

    feat: configure twig template engine and view method

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -5,10 +5,20 @@ namespace App\Controllers;
 
 class BaseController 
 {
- 
+    protected $templateEngine;
+
     public function __construct()
     {
-      
+        $loader = new \Twig\Loader\FilesystemLoader('../views');
+        $this->templateEngine = new \Twig\Environment($loader,[
+            'debug' => true,
+            'cache' => false
+        ]);
 
+    }
+
+    public function view($fileName, $data = [])
+    {
+        return $this->templateEngine->render($fileName, $data);
     }
 }
