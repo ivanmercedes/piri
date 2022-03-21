99e5bce68a687a2a97a2f45647ffdbca6cf1cdb4
Ivan Mercedes <ivanmercede@gmail.com>
2022-03-21 10:03:53 -0400

    feat: config dotenv and initial route

--- public/index.php
+++ public/index.php
@@ -2,4 +2,19 @@
 
 require('../vendor/autoload.php');
 
+use Phroute\Phroute\RouteCollector;
 
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/..');
+$dotenv->load();
+
+$route = $_GET['route'] ?? '/';
+$router = new RouteCollector();
+
+$router->controller('/', App\Controllers\HomeController::class);
+
+$dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
+$response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], $router);
+
+
+echo $response;
